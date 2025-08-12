006c0a7057b3c1fc0f1e07f54cce4ce868d74f83
Mirko <45468712+mirko1075@users.noreply.github.com>
2025-08-12 20:32:24 +0200

    Fix Netlify deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
   env: {
     NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:3000/api/v1',
   },
